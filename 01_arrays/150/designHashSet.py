cbcd63cc560787c896c2513c4274a81c09116bdc
Nicholas <contactnicholas81@gmail.com>
2026-06-18 18:20:14 +0100

    designHashSet

---
+++ 01_arrays/150/designHashSet.py
@@ -0,0 +1,22 @@
+class MyHashSet:
+    def __init__(self):
+        self.store = []
+
+    def add(self, key: int) -> None:
+        if not key in self.store:
+            self.store.append(key)
+        
+    def remove(self, key: int) -> None:
+        if key in self.store:
+            self.store.remove(key)
+
+    def contains(self, key: int) -> bool:
+        if key in self.store:
+            return True
+        return False
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
