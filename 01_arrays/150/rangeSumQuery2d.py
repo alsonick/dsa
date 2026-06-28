216eba1a21c3807d4e298a640b75960321cc8527
Nicholas <contactnicholas81@gmail.com>
2026-06-28 13:44:03 +0100

    rangeSumQuery2d

---
+++ 01_arrays/150/rangeSumQuery2d.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        # !- NeetCode Solution -!
+        rows, cols = len(matrix), len(matrix[0])
+        self.matrix = [[0] * (cols + 1) for c in range(rows + 1)]
+        for r in range(rows):
+            prefix = 0
+            for c in range(cols):
+                prefix += matrix[r][c]
+                above = self.matrix[r][c + 1]
+                self.matrix[r + 1][c + 1] = prefix + above
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        bottomRight = self.matrix[r2][c2]
+        above = self.matrix[r1 - 1][c2]
+        left = self.matrix[r2][c1 - 1]
+        topLeft = self.matrix[r1 - 1][c1 - 1]
+
+        return bottomRight - above - left + topLeft
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
